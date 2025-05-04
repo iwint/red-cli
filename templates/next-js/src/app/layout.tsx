5af62781a128527ae73a55523565b24e1ec2a4dd
iwint <iwint.cseasnsct@gmail.com>
2025-05-04 21:25:34 +0530

    Remove unused font files and update global styles to use Google Fonts. Refactor layout to eliminate local font imports.

--- templates/next-js/src/app/layout.tsx
+++ templates/next-js/src/app/layout.tsx
@@ -1,19 +1,7 @@
 import type { Metadata } from 'next';
-import localFont from 'next/font/local';
 import './globals.css';
 import { Providers } from '@/providers';
 import { Suspense } from 'react';
-
-const geistSans = localFont({
-    src: './fonts/GeistVF.woff',
-    variable: '--font-geist-sans',
-    weight: '100 900',
-});
-const geistMono = localFont({
-    src: './fonts/GeistMonoVF.woff',
-    variable: '--font-geist-mono',
-    weight: '100 900',
-});
 
 export const metadata: Metadata = {
     title: 'Red ClI Next Js',
@@ -27,7 +15,7 @@ export default function RootLayout({
 }>) {
     return (
         <html lang="en">
-            <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
+            <body>
                 <Providers>
                     <Suspense fallback={<div>Loading...</div>}>{children}</Suspense>
                 </Providers>
